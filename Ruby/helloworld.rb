44aaf8cc0c471d85ed69dc8f3374d751d3b6e934
fumi2323 <fuminori.kurita@gmail.com>
2017-07-20 14:24:10 +0900

    Rubyの入り口

---
+++ Ruby/helloworld.rb
@@ -0,0 +1,13 @@
+#! /usr/local/bin/ruby
+
+print "Content-type: text/html\n\n"
+print "<html>\n"
+print "<head>\n"
+print "<title>Hello world!</title>\n"
+print "</head>\n"
+print "<body>\n"
+print "Hello world!<br>\n"
+print "</body>\n"
+print "</html>\n"
+
+exit
